503ec5467ea6390d5aa03d0009eb5d2437df12ad
Leon Liu <lliu@fnz.com>
2021-09-06 17:42:00 +0800

    Inject LazyAssemblyLoader service;

--- BlazorAppLazyAssembly/Program.cs
+++ BlazorAppLazyAssembly/Program.cs
@@ -1,9 +1,11 @@
 using BlazorAppLazyAssembly;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.AspNetCore.Components.WebAssembly.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped<LazyAssemblyLoader>();
 
 await builder.Build().RunAsync();
